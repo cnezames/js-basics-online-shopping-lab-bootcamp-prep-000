7ab9fcc635dffc1b3706691bcf55d4d7e4b7b151
Cynthia Nezames <cnezames@gmail.com>
2018-08-21 17:11:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,9 +46,15 @@ function total() {
 }
 
 function removeFromCart(item) {
-  
-  cart.splice(cart.indexOf(itemPrice.item), 1)
-  return cart
+  var i = 0
+  while (i < cart.length) {
+    if (cart[i].itemName === item) {
+      cart.splice(i, 1)
+      return cart
+    }
+    else i++
+  }
+  return "That item is not in your cart."
 }
 
 
